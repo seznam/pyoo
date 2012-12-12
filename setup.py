2b4cc6a5ed3ef48f085591bf88f96441e4a4694a
Miloslav Pojman <miloslav.pojman@firma.seznam.cz>
2012-12-12 18:01:00 +0100

    Add simple setup.py script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name='pyoo',
+    version='0.1',
+    description='Pythonic interface to OpenOffice.org API known as UNO.',
+    author='Miloslav Pojman',
+    author_email='miloslav.pojman@firma.seznam.cz',
+    url='http://cml.kancelar.seznam.cz/generic/browser/szn-python-pyoo',
+    py_modules=['pyoo'],
+
+)
