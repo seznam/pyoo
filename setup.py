12550873defb47f404b674d574141723537ac499
Miloslav Pojman <miloslav.pojman@gmail.com>
2014-07-11 15:49:35 +0200

    Set version to 1.0 and update debianisation.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 
 setup(
     name='pyoo',
-    version='0.4',
+    version='1.0',
     description='Pythonic interface to Apache OpenOffice API (UNO)',
     long_description = open('README.rst').read(),
     author='Miloslav Pojman',
