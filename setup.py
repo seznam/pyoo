b98b81218643ce0b08aa97a2370df2216e095820
Miloslav Pojman <miloslav.pojman@gmail.com>
2014-07-11 14:07:06 +0200

    Better setup.py script.

--- setup.py
+++ setup.py
@@ -11,9 +11,16 @@ from distutils.core import setup
 setup(
     name='pyoo',
     version='0.4',
-    description='Pythonic interface to OpenOffice.org API (UNO)',
+    description='Pythonic interface to Apache OpenOffice API (UNO)',
     long_description = open('README.rst').read(),
     author='Miloslav Pojman',
     author_email='miloslav.pojman@firma.seznam.cz',
     py_modules=['pyoo'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Office/Business :: Office Suites',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
 )
